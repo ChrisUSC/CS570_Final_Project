1dc5e439d1d2619cf04ad588a8ab51c809cb1630
Vinay Gupta <vinay.23.gupta@outlook.com>
2021-11-14 23:51:08 -0800

    Make profiling cross-platform, add tester

--- venv/profiler.py
+++ venv/profiler.py
@@ -1,19 +1,49 @@
+import platform
 import gc
+from sys import argv
 import timeit
-import psutil
 
-# NOTE: This is not platform-independent, currently uses Windows-specific APIs.
+PLATFORM_OS = platform.system()
+
+if PLATFORM_OS == 'Windows':
+    import psutil
+else:
+    import resource
 
 
-def run_and_profile(func, *args, **kwargs):
+def get_memory_usage():
+    if PLATFORM_OS == 'Windows':
+        return psutil.Process().memory_full_info().peak_wset / 1024
+    elif PLATFORM_OS == 'Linux':
+        return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    else:
+        return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
+
+
+def run_with_profiler(func, *args, **kwargs):
     response = {}
 
-    def wrapped_func():
-        gc.collect()
-        start_mem = psutil.Process().memory_full_info()
+    def wrapper():
+        start_mem = get_memory_usage()
         response['output'] = func(*args, **kwargs)
-        end_mem = psutil.Process().memory_full_info()
-        response['memory'] = end_mem.peak_wset - start_mem.peak_wset
+        end_mem = get_memory_usage()
+        response['memory'] = end_mem - start_mem
 
-    response['time'] = timeit.timeit(wrapped_func, number=1)
+    timer = timeit.Timer(stmt=wrapper, setup=lambda: gc.collect())
+    response['time'] = timer.timeit(number=1)
     return response
+
+
+if __name__ == '__main__':
+    import sys
+
+    def test_func(rows, cols):
+        arr = [([0] * cols) for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
+                arr[i][j] = i * j
+        return rows * cols
+
+    size = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    response = run_with_profiler(test_func, size, size)
+    print(response)
