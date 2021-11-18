e8120219e97d682468f168fbfe093556873d1039
Vinay Gupta <vinay.23.gupta@outlook.com>
2021-11-17 16:49:17 -0800

    Add the top-level flow for the D&C algorithm

--- venv/efficient.py
+++ venv/efficient.py
@@ -1,4 +1,5 @@
 from alignment_params import calculate_alpha, GAP_PENALTY
+from basic import basic_solution
 
 '''
 Pseudocode from text book
@@ -81,12 +82,43 @@ Divide-and-conquer-alignment(X,Y):
 '''
 
 
-def efficient_solution(string1, string2):
+def find_alignment_scores(string1, string2):
+    # Return the last column with scores
+    # That is nothing but the scores for all of string1, against each substring of string2
+    # The returned array should have size len(string2) x 1
     gap_penalty = GAP_PENALTY
-    # TODO
+    pass
+
+
+def efficient_solution(string1, string2):
+    m = len(string1)
+    n = len(string2)
+
+    # Base case
+    if m <= 2 or n <= 2:
+        # TODO: Is it possible that either m or n become 0?
+        return basic_solution(string1, string2)
+
+    # Find the scores for left half and right half
+    mid = m // 2
+    scores_left = find_alignment_scores(string1[:mid], string2)
+    scores_right = find_alignment_scores(string1[-1:mid-1:-1], string2[-1::-1])
+
+    # Find the divide point in string2
+    split = 0
+    min_score = scores_left[0] + scores_right[0]
+    for i in range(1, len(string2)):
+        score = scores_left[i] + scores_right[i]
+        if score < min_score:
+            min_score = score
+            split = i
+
+    # Solve sub-problems and combine the solution
+    align1_left, align2_left = efficient_solution(string1[:mid], string2[:split])
+    align1_right, align2_right = efficient_solution(string1[mid:], string2[split:])
+    return align1_left + align1_right, align2_left + align2_right
 
 
 if __name__ == "__main__":
     import generate_result as gr
-
     gr.parse_cli_and_run(efficient_solution)
